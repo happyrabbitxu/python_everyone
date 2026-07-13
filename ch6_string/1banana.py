c774daa4e2b8b7ba02c2e1567cd7a6deca43234d
happyrabbit_xu <bingtangxiaoshui@outlook.com>
2026-07-13 14:55:18 +0800

    new

---
+++ ch6_string/1banana.py
@@ -0,0 +1,7 @@
+fruit = 'banana'
+letter = fruit[1]  #computer always starts counting from 0 so the second letter is at index 1
+print(letter)
+x = 3
+w = fruit[x-1]
+print(w)
+print(len(fruit))
---
+++ ch6_string/3looping_through_string2.py
@@ -0,0 +1,4 @@
+fruit = 'banana'
+for letter in fruit:
+    print(letter)
+#An easy way to loop through a string is to use a for loop. The for loop automatically goes through each letter in the string without needing to use an index variable.
