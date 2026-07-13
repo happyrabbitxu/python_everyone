badb151717b65f15f7d376606fb9d085bbab254f
happyrabbit_xu <bingtangxiaoshui@outlook.com>
2026-07-13 15:34:50 +0800

    new

---
+++ ch6_string/7functions.py
@@ -0,0 +1,5 @@
+greet = 'Hello Bob'
+zap = greet.lower() #lower() method returns a copy of the string in which all case-based characters have been lowercased and return it to us.
+print(zap)
+print(greet)
+print('Hi There'.lower())
---
+++ ch6_string/methods.py
@@ -0,0 +1,3 @@
+stuff = 'Hello World!'
+print(type(stuff))
+print(dir(stuff))   #This will show us all the methods that are available for strings. The dir() function returns a list of valid attributes(有效属性) for that object.
